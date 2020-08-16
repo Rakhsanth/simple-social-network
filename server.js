05d723ee6d46904f600bb8c25caa476498c002bc
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2020-08-16 16:16:33 +0530

    initial setup

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 // 3rd party modules
 const express = require('express');
+const morgan = require('morgan');
+const colors = require('colors');
 
 const app = express();
+
+app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan());
+}
 
 app.get('/', (request, response) => response.send('App is rinnung'));
 
 const PORT = process.env.PORT || 4010;
 
-app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server is running on the PORT: ${PORT}`.yellow.bold)
+);
