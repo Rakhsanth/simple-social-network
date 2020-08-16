236df1ce51e709da8bfcce9bcb94920df95effdd
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2020-08-16 15:58:03 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,8 @@
+// 3rd party modules
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
