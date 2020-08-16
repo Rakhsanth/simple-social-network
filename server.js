b2a96864d18dc15e23267238d88fe2f8b6be7148
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2020-08-16 16:00:00 +0530

    initial setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => response.send('App is rinnung'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
