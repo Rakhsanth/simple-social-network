3e82a105aebd9319bdf04832bcbc02c4205dcbf5
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2021-10-01 13:13:36 +0530

    Configuring cors

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ dotenv.config({
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
 connectDB();
 
 if (process.env.NODE_ENV === 'development') {
